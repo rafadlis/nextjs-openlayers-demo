bfb36ed73e241f92f36a84c10eca7dbfbfc64fe8
rafadlis <rahmatfadlis@gmail.com>
2025-11-09 12:17:36 +0700

    init

---
+++ app/_components/map.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import { Draw, Modify, Snap } from "ol/interaction";
+import TileLayer from "ol/layer/Tile";
+import VectorLayer from "ol/layer/Vector";
+import OlMap from "ol/Map";
+import { fromLonLat } from "ol/proj";
+import OSM from "ol/source/OSM";
+import VectorSource from "ol/source/Vector";
+import View from "ol/View";
+import { useEffect, useRef } from "react";
+import "ol/ol.css";
+
+export default function MapComponent() {
+  const mapRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    if (!mapRef.current) {
+      return;
+    }
+
+    const baseLayer = new TileLayer({
+      source: new OSM(),
+    });
+    const drawSource = new VectorSource();
+    const drawLayer = new VectorLayer({
+      source: drawSource,
+    });
+    const map = new OlMap({
+      layers: [baseLayer, drawLayer],
+      controls: [],
+      target: mapRef.current,
+      view: new View({
+        center: fromLonLat([107.940_582, -7.175_513]),
+        zoom: 12,
+      }),
+    });
+    const modifyInteraction = new Modify({ source: drawSource });
+    const drawInteraction = new Draw({ type: "Polygon", source: drawSource });
+    const snapInteraction = new Snap({ source: drawSource });
+
+    map.addInteraction(modifyInteraction);
+    map.addInteraction(drawInteraction);
+    map.addInteraction(snapInteraction);
+
+    const handleKeyDown = (event: KeyboardEvent): void => {
+      if (event.key === "Escape" || event.key === "Esc") {
+        drawInteraction.abortDrawing();
+        return;
+      }
+
+      // Handle Ctrl+Z (Windows/Linux) and Cmd+Z (macOS)
+      if (event.key.toLowerCase() === "z" && (event.ctrlKey || event.metaKey)) {
+        event.preventDefault();
+        drawInteraction.removeLastPoint();
+      }
+    };
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+      map.setTarget(undefined);
+    };
+  }, []);
+  return (
+    <div className="relative">
+      <div ref={mapRef} style={{ width: "100%", height: "100vh" }} />
+    </div>
+  );
+}
